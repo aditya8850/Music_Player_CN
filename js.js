15dbd082cd18b6877aa8df7769cad23ce1a68c9f
aditya kalsotra <kalsotraaditya98@gmail.com>
2024-03-19 17:33:44 +0530

    styling

--- js.js
+++ js.js
@@ -10,8 +10,8 @@ const songs = [
     ,
     {
         id: 2,
-        name: "heaven",
-        artist: "eminem",
+        name: "Heaven",
+        artist: "Eminem",
         img: "thumbnails/Eminem-Heaven.webp",
         genre: "hiphop",
         source: "audioSRC/eminem-heaven.mp3",
@@ -19,8 +19,8 @@ const songs = [
     ,
     {
         id: 3,
-        name: "neurotica",
-        artist: "polyphia",
+        name: "Neurotica",
+        artist: "Polyphia",
         img: "thumbnails/polyphia-pg",
         genre: "metal",
         source: "audioSRC/polyphia-neurotica.mp3",
@@ -28,7 +28,7 @@ const songs = [
     ,
     {
         id: 4,
-        name: "without me",
+        name: "Without me",
         artist: "halsey ft Juice Wrld",
         img: "thumbnails/Without-Me.png",
         genre: "pop",
@@ -37,8 +37,8 @@ const songs = [
     ,
     {
         id: 5,
-        name: "all time low",
-        artist: "jon bellion",
+        name: "All time low",
+        artist: "jon Bellion",
         img: "thumbnails/All-Time-Low.png",
         genre: "pop",
         source: "audioSRC/all-time-low.mp3",
@@ -68,9 +68,10 @@ showSongs(songs)
 function showSongs(songs) {
     //showing list of songs on the first card
     const listOfSongs = document.querySelector('.list-inside');
-    listOfSongs.classList.add('cursor-pointer')
+    
     songs.forEach((song) => {
         const pInLists = document.createElement('p');
+        pInLists.classList.add('cursor-pointer')
         pInLists.innerHTML = song.name
         // console.log(listOfSongs)
         pInLists.innerHTML = `${song.artist}: ${song.name}`
