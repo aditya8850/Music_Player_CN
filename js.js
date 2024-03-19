e841d4c8d3db8a5559400cbabe94d24ff817f2b1
aditya kalsotra <kalsotraaditya98@gmail.com>
2024-03-19 16:16:51 +0530

    updated card functionality

--- js.js
+++ js.js
@@ -1,25 +1,29 @@
-const songs= [
-    {id:1,
-    name: "Time",
-    artist:"Nf",
-    img:"/thumbnails/NF-Time.webp",
-    genre:"HipHop",
-    source:"/audioSRC/nf-time.mp3",}
+const songs = [
+    {
+        id: 1,
+        name: "Time",
+        artist: "Nf",
+        img: "/thumbnails/NF-Time.webp",
+        genre: "hiphop",
+        source: "/audioSRC/nf-time.mp3",
+    }
     ,
-    {id:2,
-    name:"heaven",
-    artist:"eminem",
-    img:"/thumbnails/Eminem-Heaven.webp",
-    genre:"HipHop",
-    source:"/audioSRC/eminem- heaven.mp3",}
+    {
+        id: 2,
+        name: "heaven",
+        artist: "eminem",
+        img: "/thumbnails/Eminem-Heaven.webp",
+        genre: "hiphop",
+        source: "/audioSRC/eminem- heaven.mp3",
+    }
     ,
-    // {id:3,
-    // name:,
-    // artist:,
-    // img:,
-    // genre:,
-    // source:,}
-    // ,
+    {id:3,
+    name:"neurotica",
+    artist:"polyphia",
+    img:"/thumbnails/polyphia-pg",
+    genre:"metal",
+    source:"/audioSRC/polyphia -neurotica.mp3",}
+    ,
     // {id:4,
     // name:,
     // artist:,
@@ -45,45 +49,47 @@ const songs= [
 
 //function for toggle-switch
 toggleSwitch()
-function toggleSwitch(){const toggleSwitch = document.getElementById('toggle-switch-button');
-const body = document.querySelector('body');
+function toggleSwitch() {
+    const toggleSwitch = document.getElementById('toggle-switch-button');
+    const body = document.querySelector('body');
 
-toggleSwitch.addEventListener('click', () => {
-    body.classList.toggle('white-background');
-});}
+    toggleSwitch.addEventListener('click', () => {
+        body.classList.toggle('white-background');
+    });
+}
 
 //function for showing songs
 
 showSongs()
-function showSongs(){
+function showSongs() {
     //showing list of songs on the first card
-const listOfSongs= document.querySelector('.list-inside');
-listOfSongs.classList.add('cursor-pointer')
-songs.forEach((song)=>{
-    const pInLists= document.createElement('p');
-    pInLists.innerHTML=song.name
-    // console.log(listOfSongs)
-    pInLists.innerHTML= `${song.artist}: ${song.name}`
-    listOfSongs.appendChild(pInLists);
-    
-    pInLists.addEventListener('click',function(e){
-        const thumbnail= document.querySelector(".thumbnail-song");
-       (thumbnail.src)= song.img;
-       
-       const name= document.querySelector('#current-song');
-       name.innerHTML= song.name;
-    
-       const audio= document.querySelector('.current-song-audio');
-       audio.src= song.source;
-       audio.autoplay=true;
-       
-    
-       });
-})
+    const listOfSongs = document.querySelector('.list-inside');
+    listOfSongs.classList.add('cursor-pointer')
+    songs.forEach((song) => {
+        const pInLists = document.createElement('p');
+        pInLists.innerHTML = song.name
+        // console.log(listOfSongs)
+        pInLists.innerHTML = `${song.artist}: ${song.name}`
+        listOfSongs.appendChild(pInLists);
+
+        pInLists.addEventListener('click', function (e) {
+            const thumbnail = document.querySelector(".thumbnail-song");
+            (thumbnail.src) = song.img;
+
+            const name = document.querySelector('#current-song');
+            name.innerHTML = song.name;
+
+            const audio = document.querySelector('.current-song-audio');
+            audio.src = song.source;
+            audio.autoplay = true;
+
+
+        });
+    })
 
 
 
-//adding e-listener to update thumbnail ,name and audio src.
+    //adding e-listener to update thumbnail ,name and audio src.
 
 
 }
